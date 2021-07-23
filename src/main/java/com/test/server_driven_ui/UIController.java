3b99682078ec4c8446cb373c89addaa7d9175b8a
Saurav Munankarmi <sauravmunankarmi@gmail.com>
2021-07-23 23:03:09 +0545

    dynamic response body with hashmap added

--- src/main/java/com/test/server_driven_ui/UIController.java
+++ src/main/java/com/test/server_driven_ui/UIController.java
@@ -6,22 +6,38 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 @RestController
 @RequestMapping("api/v1")
 public class UIController {
 
     @GetMapping("/ui")
     public ResponseEntity<?> getUI(){
-        ExampleLayout exampleLayout = new ExampleLayout(
-                "TextView",
-                "300dp",
-                "300dp",
-                "@{username}"
-        );
+//        ExampleLayout exampleLayout1 = new ExampleLayout(
+//                "TextView",
+//                "300dp",
+//                "300dp",
+//                "@{username}"
+//
+//        );
+//
+//        ExampleData exampleData1 = new ExampleData(
+//                "saurav"
+//        );
 
-        ExampleData exampleData = new ExampleData(
-                "saurav"
-        );
+        LinkedHashMap<String,String> exampleLayout = new LinkedHashMap<>();
+        exampleLayout.put("type","TextView");
+        exampleLayout.put("layout_width","match_parent");
+        exampleLayout.put("layout_height","wrap_content");
+        exampleLayout.put("text","@{username}");
+//        exampleLayout.put("singleLine","@bool/true");
+        exampleLayout.put("gravity","center");
+        exampleLayout.put("textColor","#ff0000");
+
+        LinkedHashMap<String,String> exampleData = new LinkedHashMap<>();
+        exampleData.put("username","saurav");
 
         return new ResponseEntity<Response>(new Response(exampleLayout, exampleData), HttpStatus.OK);
     }
