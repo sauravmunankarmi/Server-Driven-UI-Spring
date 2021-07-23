f754987a7a7b2d54154c524762ea7180b82a2d5d
Saurav Munankarmi <sauravmunankarmi@gmail.com>
2021-07-23 23:49:09 +0545

    minor changes

--- src/main/java/com/test/server_driven_ui/UIController.java
+++ src/main/java/com/test/server_driven_ui/UIController.java
@@ -31,41 +31,61 @@ public class UIController {
 
 
 
+        //text view 1
         LinkedHashMap<String,Object> exampleTextView = new LinkedHashMap<>();
         exampleTextView.put("type","TextView");
         exampleTextView.put("layout_width","match_parent");
         exampleTextView.put("layout_height","wrap_content");
         exampleTextView.put("text","@{username}");
-//        exampleTextView.put("singleLine","@bool/true");
         exampleTextView.put("gravity","center");
         exampleTextView.put("textColor","#ff0000");
         exampleTextView.put("elevation","4dp");
 
+        //text view 2 which is to be below text view 1
         LinkedHashMap<String,Object> exampleTextView2 = new LinkedHashMap<>();
         exampleTextView2.put("type","TextView");
         exampleTextView2.put("layout_width","match_parent");
         exampleTextView2.put("layout_height","wrap_content");
         exampleTextView2.put("text","@{userlastname}");
-//        exampleTextView2.put("singleLine","@bool/true");
         exampleTextView2.put("gravity","center");
         exampleTextView2.put("textColor","#ff0000");
         exampleTextView2.put("elevation","4dp");
 
+        //text view 1 and 2 grouped as list of hashmap to be used in inner layout
         List<LinkedHashMap<String,Object>> childrenList = new ArrayList<>();
 
+        //text view 1 and 2 add gareko
         childrenList.add(exampleTextView);
         childrenList.add(exampleTextView2);
 
+        // inner layout hashmap ///list nabanai kana chadaina raixa
+        List<LinkedHashMap<String,Object>> childrenLayoutList = new ArrayList<>();
+
+        //inner layout green color wala
+        LinkedHashMap<String,Object> exampleLayout2 = new LinkedHashMap<>();
+        exampleLayout2.put("type","LinearLayout");
+        exampleLayout2.put("layout_width","400dp");
+        exampleLayout2.put("layout_height","200dp");
+        exampleLayout2.put("background","#00FF11");
+        exampleLayout2.put("orientation","vertical");
+        exampleLayout2.put("layout_marginTop","32dp");
+        exampleLayout2.put("layout_gravity","center");
+        exampleLayout2.put("children",childrenList);
+
+        //adding inner layout in list of hashmap ///again, hashmap ko list nabanaikana chalena
+        childrenLayoutList.add(exampleLayout2);
+
+        //root layout which contains inner layout and inner layout contains two textview
         LinkedHashMap<String,Object> exampleLayout = new LinkedHashMap<>();
         exampleLayout.put("type","LinearLayout");
         exampleLayout.put("layout_width","match_parent");
         exampleLayout.put("layout_height","match_parent");
         exampleLayout.put("background","#0088FF");
         exampleLayout.put("orientation","vertical");
-        exampleLayout.put("children",childrenList);
+        exampleLayout.put("children",childrenLayoutList);
 
 
-
+        //data wala hashmap
         LinkedHashMap<String,String> exampleData = new LinkedHashMap<>();
         exampleData.put("username","saurav");
         exampleData.put("userlastname","munankarmi");
